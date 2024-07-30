6f101e77848da51500705ff0b5abda1b535e7404
Kunal Kongkan Kashyap <122306006+kunalkcube@users.noreply.github.com>
2024-07-31 03:26:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -158,7 +158,7 @@ function App() {
   };
 
   return (
-    <div className="w-screen h-full overflow-hidden relative">
+    <div className="w-full h-full overflow-hidden relative">
       <div
         ref={mapContainer}
         className="w-full h-full"
